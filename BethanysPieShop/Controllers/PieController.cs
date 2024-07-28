a57535068cd999ef842b7a9edcdc145ee625b82c
Sami Akkawi <sami@akkawi.ch>
2024-07-28 10:10:35 +0200

    Add First Controller

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository) : Controller
+{
+    public IActionResult List()
+    {
+        return View(pieRepository.AllPies);
+    }
+}
