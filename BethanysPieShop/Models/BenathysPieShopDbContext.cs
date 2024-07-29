eb951d110f5ce3aaa66e7a335c97ff7c7ab46044
Sami Akkawi <sami@akkawi.ch>
2024-07-29 08:42:35 +0200

    Add DbContext

---
+++ BethanysPieShop/Models/BenathysPieShopDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models;
+
+public class BenathysPieShopDbContext(DbContextOptions<BenathysPieShopDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Pie> Pies { get; set; }
+}
