53b726014311f263802e2d1fb8b9ce413bc9fb61
Sami Akkawi <sami@akkawi.ch>
2024-08-07 06:28:11 +0200

    Add ShppoingCarSummary ViewComponent

---
+++ BethanysPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Components;
+
+public class ShoppingCartSummary(IShoppingCart shoppingCart) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        List<ShoppingCartItem> items = shoppingCart.GetShoppingCartItems();
+        shoppingCart.ShoppingCartItems = items;
+
+        ShoppingCartViewModel shoppingCartViewModel = new(shoppingCart);
+
+        return View(shoppingCartViewModel);
+    }
+}
