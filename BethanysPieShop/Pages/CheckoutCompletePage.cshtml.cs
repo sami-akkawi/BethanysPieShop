bfb217ba25c694bd99d1e4b82e38cb603dd3ba27
Sami Akkawi <sami@akkawi.ch>
2024-08-11 08:30:42 +0200

    Create ChecoutPage using Razor Pages

---
+++ BethanysPieShop/Pages/CheckoutCompletePage.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShop.Pages;
+
+public class CheckoutCompletePageModel : PageModel
+{
+    public void OnGet()
+    {
+        ViewData["CheckoutCompleteMessage"] = "Thanks for your order. You'll soon enjoy our delicious pies!";
+    }
+}
---
+++ BethanysPieShop/Pages/CheckoutPage.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShop.Pages;
+
+public class CheckoutPageModel(IOrderRepository orderRepository, IShoppingCart shoppingCart) : PageModel
+{
+    [BindProperty]
+    public Order Order { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        List<ShoppingCartItem> items = shoppingCart.GetShoppingCartItems();
+        shoppingCart.ShoppingCartItems = items;
+
+        if (shoppingCart.ShoppingCartItems.Count == 0)
+        {
+            ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            return Page();
+        }
+        
+        orderRepository.CreateOrder(Order);
+        shoppingCart.ClearCart();
+        
+        return RedirectToPage("CheckoutCompletePage");
+    }
+}
