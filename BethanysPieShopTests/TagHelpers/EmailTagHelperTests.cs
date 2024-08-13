7c0acc00fe01cdef85fd428cf132e545ac041dba
Sami Akkawi <sami@akkawi.ch>
2024-08-13 04:53:13 +0200

    Add EmailTagHelperTests

---
+++ BethanysPieShopTests/TagHelpers/EmailTagHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using BethanysPieShop.TagHelpers;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+
+namespace BethanysPieShopTests.TagHelpers;
+
+public class EmailTagHelperTests
+{
+    [Fact]
+    public void Generates_Email_Link()
+    {
+        EmailTagHelper emailTagHelper = new()
+        {
+            Address = "test@bethanyspieshop.com",
+            Content = "Email"
+        };
+
+        TagHelperContext tagHelperContext =
+            new(new TagHelperAttributeList(), new Dictionary<object, object>(), string.Empty);
+
+        TagHelperContent tagHelperContent = new Mock<TagHelperContent>().Object;
+
+        TagHelperOutput tagHelperOutput = new("a", new TagHelperAttributeList(),
+            (_, _) => Task.FromResult(tagHelperContent));
+        
+        emailTagHelper.Process(tagHelperContext, tagHelperOutput);
+        
+        Assert.Equal("Email", tagHelperOutput.Content.GetContent());
+        Assert.Equal("a", tagHelperOutput.TagName);
+        Assert.Equal("mailto:test@bethanyspieshop.com", tagHelperOutput.Attributes[0].Value);
+    }
+}
