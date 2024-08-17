06d6360a8ead7218ca4d7d36c85096e7ee9dd065
Sami Akkawi <sami@akkawi.ch>
2024-08-17 05:10:24 +0200

    Add Blazor functionality support

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(ShoppingCart.GetCart);
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.UseStaticFiles();
 app.UseSession();
@@ -36,8 +36,12 @@ if (app.Environment.IsDevelopment())
 // required for API, not needed here because we use MapDefaultControllerRoute
 // app.MapControllers();
 
+app.UseAntiforgery();
+
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
+app.MapRazorComponents<WebApplication>()
+    .AddInteractiveServerRenderMode();
 
 DbInitializer.Seed(app);
 
